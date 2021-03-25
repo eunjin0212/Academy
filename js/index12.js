080f2c0ca15cd98e795f32d508aad49c81f5660d
eunjin0212 <eunjin9639@gmail.com>
2021-03-25 15:50:13 +0900

    readOnly

--- js/index12.js
+++ js/index12.js
@@ -2,8 +2,12 @@ function telP() {
   document.getElementById("tel").value = document.getElementById("cp").value;
 }
 
-function emailFunc() {
-  document.getElementById("email").value = document.getElementById(
-    "emailSel"
-  ).value;
+function emailFunc(value) {
+  let email = document.getElementById("email");
+  if (value === "my") {
+    email.readOnly = false;
+  } else {
+    email.value = value;
+    email.readOnly = true;
+  }
 }
