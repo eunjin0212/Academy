78fb7b999d0c8dd0b482a1ff2f7246e3aacc363f
eunjin0212 <eunjin9639@gmail.com>
2021-03-25 15:40:33 +0900

    Select onClick

---
+++ js/index12.js
@@ -0,0 +1,9 @@
+function telP() {
+  document.getElementById("tel").value = document.getElementById("cp").value;
+}
+
+function emailFunc() {
+  document.getElementById("email").value = document.getElementById(
+    "emailSel"
+  ).value;
+}
