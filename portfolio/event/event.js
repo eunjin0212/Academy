72084bfe0938f74a5c8a30c506a8429d28fbdca5
eunjin0212 <eunjin9639@gmail.com>
2021-04-26 10:26:12 +0900

    Modified event function rotate degree, Add click count

--- portfolio/event/event.js
+++ portfolio/event/event.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   let $img;
-  let $data = [5000, 100, 200, 2000, 1000, 100, 500, 300];
+  let $data = [5000, 100, 200, 2000, 1000, 500, 300];
 
   $("#game_btn").mouseover(function () {
     $("#game_btn>img").attr("src", "./img/event_start_on.png");
@@ -13,26 +13,33 @@ $(document).ready(function () {
   let $location = 0; //오브젝트 최초 위치
 
   $("#game_btn").click(function () {
-    Number($("#ct").val()) - 1;
-    if (Number($("#ct").val()) == 0) {
-      $(".game_pan").css("display", "none");
-      alert("아쉽지만 기회 끝!");
-    } else {
+    let $checkCount = Number($("#ct").val());
+
+    if ($checkCount > 0) {
+      $checkCount -= 1;
+      $("#ct").val($checkCount);
+
       $("#msg").slideUp(800);
       let $randomNum = Math.ceil(Math.random() * 360);
       $location = $location + 1800; //도는 횟수 5바퀴
       let $msg = $.fn.rotate($location, $randomNum);
+
       setTimeout(function () {
         $("#alert").slideDown(800);
-        $("#alert").html("당첨 " + $data[$msg] + " 마일리지");
-        console.log("msg", $msg, "data", $data);
+        $("#alert").html("당첨 " + $data[$msg] + " 마일리지!!");
+        console.log($data[$msg]);
       }, 5500);
+    } else {
+      alert("아쉽지만 기회 끝!");
+      $(".container").css("display", "none");
+      $("#alert").slideUp(800);
     }
   });
 
   $.fn.rotate = function ($location, $randomNum) {
     let $node = 0;
-    if ($randomNum > 24 && $randomNum <= 65) {
+    console.log($randomNum);
+    if ($randomNum >= 25 && $randomNum <= 65) {
       $node = 1;
     } else if ($randomNum >= 70 && $randomNum <= 110) {
       $node = 2;
@@ -46,12 +53,13 @@ $(document).ready(function () {
       $node = 5;
     } else if ($randomNum >= 295 && $randomNum <= 335) {
       $node = 6;
-    } else if ($randomNum >= 340 && $randomNum <= 360) {
-      $node = 7;
-    } else if ($randomNum == 0 && $randomNum < 25) {
+    } else if (
+      ($randomNum >= 340 && $randomNum <= 360) ||
+      ($randomNum == 0 && $randomNum < 25)
+    ) {
       $node = 0;
     } else {
-      $node = 0;
+      $node = 1;
     }
     let $rotates = $location + $randomNum;
     $(".game_pan").css({ transform: "rotate(" + $rotates + "deg)" });
