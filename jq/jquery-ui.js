b70083f4bb7b2e04685db5521a4f3e751a461163
eunjin0212 <eunjin9639@gmail.com>
2021-05-10 18:40:05 +0900

    Add jquery file

--- jq/jquery-ui.js
+++ jq/jquery-ui.js
@@ -10286,7 +10286,7 @@
       // +/-number for offset from today, null for today
       appendText: "", // Display text following the input box, e.g. showing the format
       buttonText: "일정표시", // Text for trigger button
-      buttonImage: "./cal.svg", // URL for trigger button image
+      buttonImage: "../cal.svg", // URL for trigger button image
       buttonImageOnly: true, // True if the image appears alone, false if it appears on a button
       hideIfNoPrevNext: false, // True to hide next/previous month links
       // if not applicable, false to just disable them
