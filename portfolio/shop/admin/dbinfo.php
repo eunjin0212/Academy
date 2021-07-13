1c50cbc9ef48bed724d6343d2d65eeeadd4aaa39
eunjin0212 <eunjjin9639@gmail.com>
2021-07-13 09:48:01 +0900

    portfoilo php

--- portfolio/shop/admin/dbinfo.php
+++ portfolio/shop/admin/dbinfo.php
@@ -8,3 +8,7 @@ $dbname = "eunjin9639";
 $connect = mysqli_connect($host,$user,$pwd) or die("데이터베이스 접속오류!!");
 mysqli_select_db($connect,$dbname);
 ?>
+<!-- $host = "localhost";
+$user = "mijisky"; 
+$pwd = "qq900202!";
+$dbname = "mijisky"; -->
---
+++ portfolio/shop/admin/memberok.php
@@ -0,0 +1,1 @@
+d:\study\html\curriculum\portfolio\shop\admin\memberok.php
