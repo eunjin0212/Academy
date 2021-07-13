2e902ae22997d9a96d21bc82e826b81787938781
eunjin0212 <eunjjin9639@gmail.com>
2021-07-13 20:14:13 +0900

    Add admin_banner

---
+++ portfolio/shop/admin/js/banner.js
@@ -0,0 +1,15 @@
+Vue.component('', {
+  template: '',
+  props: [''],
+})
+let vuu_banner = new Vue({
+  el: '#banner',
+  data: {},
+  methods: {},
+  components: {},
+  computed: {
+    abc: function () {
+      console.log(1)
+    },
+  },
+})
