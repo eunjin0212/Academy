9675bd2e046810d1afb77d4164d401a44691dd58
eunjin0212 <eunjjin9639@gmail.com>
2021-07-13 20:53:03 +0900

    정리

--- portfolio/shop/admin/js/member.js
+++ portfolio/shop/admin/js/member.js
@@ -1,34 +1,3 @@
-// 하단 버튼
-modify = function (part) {
-  switch (part) {
-    case 1:
-      if (confirm('해당 데이터를 수정하시겠습니까?')) {
-        // for(let i = 0; )
-        if (document.getElementsByClassName('all_check').checked == true) {
-          console.log('데이터 수정')
-          mfrm.action = './member_modify.php'
-          mfrm.submit()
-        } else {
-          alert('데이터를 선택해주세요.')
-          return false
-        }
-      }
-      break
-    case 2:
-      if (confirm('해당 데이터는 삭제시 복구되지 않습니다. 삭제하시겠습니까?')) {
-        mfrm.action = './member_del.php'
-        mfrm.submit()
-        history.go(0)
-      }
-      break
-    case 3:
-      if (confirm('데이터 전체 삭제를 하시겠습니까?')) {
-        console.log('전체 삭제 선택')
-      }
-      break
-  }
-}
-
 // json data 받기
 let data
 let plus = Math.floor(Math.random() * 99999)
@@ -86,25 +55,32 @@ function dataFunc(datas) {
         `<td rowspan='2'><input value='${idx}' type='checkbox' name='midx[` +
         [j] +
         `]' class='all_check' onchange='checkFunc(this.checked)' /></td>\
-          <td rowspan=2' id='user_id${idx}'>${dataArr[j]['user_id']}</td>\
-          <td class='member_name' id='user_name${idx}'>${dataArr[j]['user_name']}</td>\
-          <td colspan='6'><label for='member_identify1${idx}'><input type='radio' name='identify' id='member_identify1${idx}' /> 아이핀</label><label for='member_identify2${idx}'><input type='radio' name='identify' id='member_identify2${idx}' /> 휴대폰</label></td>\
-          <td id='user_hp${idx}'>${dataArr[j]['user_hp']}</td>\
-          <td class='levelSel'>정상<select id='levelSel${idx}'>\
-          <option value="V" '${select1}'>VIP</option>\
-          <option value="G" '${select2}'>GOLD</option>\
-          <option value="S" '${select3}'>SILVER</option>\
-          <option value="N" '${select4}'>NORMAL</option>\
-          </select></td>\
-          <td id='user_lastlog${idx}'>${dataArr[j]['user_lastlog'].split(' ')[0]}</td>\
-          <td rowspan='2' id='user_group${idx}'>${dataArr[j]['user_group']}</td>\
-          <td rowspan='2' class='user_modify' id='user_modify${idx}'><button>수정</button><button>그룹</button></td>`
+        <td rowspan=2' id='user_id${idx}'>${dataArr[j]['user_id']}</td>\
+        <td class='member_name' id='user_name${idx}'>${dataArr[j]['user_name']}</td>\
+        <td colspan='6'><label for='member_identify1${idx}'><input type='radio' name='identify' id='member_identify1${idx}' /> 아이핀</label>\
+        <label for='member_identify2${idx}'><input type='radio' name='identify' id='member_identify2${idx}' /> 휴대폰</label></td>\
+        <td id='user_hp${idx}'>${dataArr[j]['user_hp']}</td>\
+        <td class='levelSel'>정상<select id='levelSel${idx}'>\
+        <option value="V" '${select1}'>VIP</option>\
+        <option value="G" '${select2}'>GOLD</option>\
+        <option value="S" '${select3}'>SILVER</option>\
+        <option value="N" '${select4}'>NORMAL</option>\
+        </select></td>\
+        <td id='user_lastlog${idx}'>${dataArr[j]['user_lastlog'].split(' ')[0]}</td>\
+        <td rowspan='2' id='user_level${idx}'>${dataArr[j]['user_level']}</td>\
+        <td rowspan='2' class='user_modify' id='user_modify${idx}'><button>수정</button><button>그룹</button></td>`
 
-      tr2.innerHTML = `<tr><td class='member_nick' id='user_nick${idx}'>${dataArr[j]['user_nick']}</td><td class='member_mailVerify' id='mail_accept${idx}'>${
-        dataArr[j]['mail_accept']
-      }</td><td><input type='checkbox' value='Y' /></td><td><input type='checkbox' id='mail_agree${idx}' value='Y' /></td><td><input type='checkbox' id='sms_agree${idx}' value='Y' /></td><td><input type='checkbox' value='Y' /></td><td><input type='checkbox' value='Y' /></td><td id='user_tel${idx}'>${
-        dataArr[j]['user_tel']
-      }</td><td id='user_point${idx}'>${dataArr[j]['user_point']}</td><td id='user_indate${idx}'>${dataArr[j]['user_indate'].split(' ')[0]}</td></tr>`
+      tr2.innerHTML = `<tr><td class='member_nick' id='user_nick${idx}'>${dataArr[j]['user_nick']}</td>\
+        <td class='member_mailVerify' id='mail_accept${idx}'>${dataArr[j]['mail_accept']}</td>\
+        <td><input type='checkbox' value='Y' /></td>\
+        <td><input type='checkbox' id='mail_agree${idx}' value='Y' /></td>\
+        <td><input type='checkbox' id='sms_agree${idx}' value='Y' /></td>\
+        <td><input type='checkbox' value='Y' /></td>\
+        <td><input type='checkbox' value='Y' /></td>\
+        <td id='user_tel${idx}'>${dataArr[j]['user_tel']}</td>\
+        <td id='user_point${idx}'>${dataArr[j]['user_point']}</td>\
+        <td id='user_indate${idx}'>${dataArr[j]['user_indate'].split(' ')[0]}</td></tr>`
+
       memTbody.appendChild(tr1)
       memTbody.appendChild(tr2)
 
@@ -143,6 +119,37 @@ function checkFunc(ck) {
     document.getElementById('allCheck').checked = false
   }
 }
+
+// 하단 버튼
+modify = function (part) {
+  switch (part) {
+    case 1:
+      if (confirm('해당 데이터를 수정하시겠습니까?')) {
+        // for(let i = 0; )
+        if (document.getElementsByClassName('all_check').checked == true) {
+          console.log('데이터 수정')
+          mfrm.action = './member_modify.php'
+          mfrm.submit()
+        } else {
+          alert('데이터를 선택해주세요.')
+          return false
+        }
+      }
+      break
+    case 2:
+      if (confirm('해당 데이터는 삭제시 복구되지 않습니다. 삭제하시겠습니까?')) {
+        mfrm.action = './member_del.php'
+        mfrm.submit()
+      }
+      break
+    case 3:
+      if (confirm('데이터 전체 삭제를 하시겠습니까?')) {
+        console.log('전체 삭제 선택')
+      }
+      break
+  }
+}
+
 // 팝업 띄우기
 $(function () {
   $('#add_member').click(function () {
