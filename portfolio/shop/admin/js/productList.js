7ab1f19e92793711b1625cc074f1f685d93ed1c9
eunjin0212 <eunjjin9639@gmail.com>
2021-07-17 00:58:03 +0900

    검색기능 추가

--- portfolio/shop/admin/js/productList.js
+++ portfolio/shop/admin/js/productList.js
@@ -4,11 +4,37 @@ function productInput() {
 
 let productListVue = new Vue({
   el: "#product_list",
-  data: {},
-  methods: {},
+  data: {
+    bool: "",
+    searchWord: "",
+  },
+  methods: {
+    // 검색 기능
+    searchFunc() {
+      if (this.searchWord !== "") {
+        location.href = "admin_product.html?stext="
+      } else {
+        alert("단어를 입력하세요.")
+        // location.href = "admin_product.html"
+      }
+    },
+  },
   components: {},
-  computed: {},
+  computed: {
+    productDataFunc() {
+      fetch("admin_product.json?v=<?=$v?>")
+        .then((res) => {
+          return res.json()
+        })
+        .then((data) => {
+          bannerListVue.bannerFunc(data)
+        })
+        .catch((error) => {
+          error = new Error()
+          if (error == "Error") {
+            this.bool = false
+          }
+        })
+    },
+  },
 })
-// 상품 할인 유/무 : product_dc   (“Y” , ”N”)
-// 상품 할인율
-// product_dcpart	(“P” , ”M”)
